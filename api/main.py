1943756a7b9201c4329efafce8bca1422e8f8552
Błażej Radzik <blazej.radzik@edu.uekat.pl>
2026-03-23 20:20:55 +0100

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
 from api.routes import router
-
 app = FastAPI(title="QuantRisk Engine API")
-
 app.include_router(router, prefix="/v1")
-
 @app.get("/")
 def read_root():
     return {"status": "QuantRisk Engine is Online"}
